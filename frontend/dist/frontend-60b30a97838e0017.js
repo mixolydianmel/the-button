6614da6bd93c0d5803809af78f74013faf352e00
Melody <mixolydianmel@proton.me>
2023-06-05 15:45:11 +0000

    Changed endpoints for stats

--- frontend/dist/frontend-60b30a97838e0017.js
+++ frontend/dist/frontend-60b30a97838e0017.js
@@ -782,7 +782,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('frontend-1a70c23f9b418fbf_bg.wasm', import.meta.url);
+        input = new URL('frontend-60b30a97838e0017_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
--- frontend/src/components/stats/mod.rs
+++ frontend/src/components/stats/mod.rs
@@ -10,8 +10,8 @@ pub fn stats() -> Html {
     html! {
         <div class="stats">
             <h1 class="stats-header"><span>{ "Stats" }</span></h1>
-            <HighScore endpoint="http://0.0.0.0:8080/api/data/high_score" name="High Score"/>
-            <Generic endpoint="http://0.0.0.0:8080/api/data/total_clicks" name="Presses"/>
+            <HighScore endpoint="http://66.228.35.230:80/api/data/high_score" name="High Score"/>
+            <Generic endpoint="http://66.228.35.230:80/api/data/total_clicks" name="Presses"/>
         </div>
     }
 }
